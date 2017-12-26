2e7305b84c4b8090ea2d889ddc92d83f757fdf76
Martí Adrogué <marti.adrogue@gmail.com>
2017-12-26 11:57:39 +0100

    Create pages for a todo list

---
+++ src/AppBundle/Controller/TodoController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * @Route("/todos", name="todo_list")
+     */
+    public function listAction(Request $request)
+    {
+        return $this->render('todos/index.html.twig');
+    }
+
+      /**
+       * @Route("/todos/create", name="todo_create")
+       */
+      public function createAction(Request $request)
+      {
+          return $this->render('todos/create.html.twig');
+      }
+
+
+      /**
+       * @Route("/todos/edit/{id}", name="todo_edit")
+       */
+      public function editAction($id, Request $request)
+      {
+          return $this->render('todos/edit.html.twig');
+
+      }
+
+      /**
+       * @Route("/todos/details/{id}", name="todo_details")
+       */
+      public function detailsAction($id, Request $request)
+      {
+          return $this->render('todos/details.html.twig');
+      }
+}
